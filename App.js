1fcba0614a462ae3ea86f72eba690c000a3284f7
WilD <a.nazarenko@nylonfabric.ru>
2022-02-22 23:37:16 +0300

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { AddTodo } from './src/components/AddTodo';
+import { Navbar } from './src/components/Navbar';
+import { Todo } from './src/components/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    setTodos(prev => [
+      ...prev, {
+        id: Date.now().toString(),
+        title
+        }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title='Todo App' />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map((todo) => <Todo key={todo.id} todo={todo} /> )}
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20
   },
+  text: { 
+
+  }
 });
